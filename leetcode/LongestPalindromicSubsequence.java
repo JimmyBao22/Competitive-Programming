f15d802c189ab09b93f0eb5d3ca35804c1eaef16
R3KTM8 <49325953+R3KTM8@users.noreply.github.com>
2021-03-27 01:01:57 -0400

    Add files via upload

---
+++ leetcode/LongestPalindromicSubsequence.java
@@ -0,0 +1,40 @@
+
+import java.util.*;
+import java.io.*;
+
+public class LongestPalindromicSubsequence {
+
+	public static void main(String[] args) {
+
+	}
+	
+	// https://leetcode.com/problems/longest-palindromic-subsequence/
+	
+	public static int longestPalindromeSubseq(String s) {
+        char[] arr = s.toCharArray();
+		int n = arr.length;
+		int[][] dp = new int[n][n];
+
+        for (int i=0; i<n; i++) {
+			dp[i][i] = 1;
+		}
+		
+		for (int diff=1; diff<n; diff++) {
+			for (int i=0; i+diff<n; i++) {
+				int j = i+diff;
+                dp[i][j] = Math.max(dp[i+1][j], dp[i][j-1]);
+				if (arr[i] == arr[j]) {
+					dp[i][j] = Math.max(dp[i][j], dp[i+1][j-1] + 2);					
+				}
+			}
+		}
+		
+		int max=0;
+		for (int i=0; i<n; i++) {
+			for (int j=0; j<n; j++) {
+				max = Math.max(max, dp[i][j]);
+			}
+		}	
+		return max;
+	}
+}
---
+++ leetcode/LongestSubstringWithoutRepeatingCharacters.java
@@ -0,0 +1,35 @@
+
+import java.util.*;
+import java.io.*;
+
+public class LongestSubstringWithoutRepeatingCharacters {
+
+	// https://leetcode.com/problems/longest-substring-without-repeating-characters/
+	
+	public static void main(String[] args) {
+		lengthOfLongestSubstring("ynyo");
+	}
+	
+	public static int lengthOfLongestSubstring(String s) {
+        // use a queue and a hashset
+		Deque<Character> seq = new ArrayDeque<>();
+		HashSet<Character> curcontained = new HashSet<>();
+		int max = 0;
+		
+		for (int i = 0; i < s.length(); i++) {
+			seq.add(s.charAt(i));
+			if (curcontained.contains(s.charAt(i))) {
+				max = Math.max(max, curcontained.size());
+				while (seq.peek() != s.charAt(i)) {
+					Character cur = seq.poll();
+					curcontained.remove(cur);
+				}
+				seq.poll();
+				curcontained.remove(s.charAt(i));
+			}
+			curcontained.add(s.charAt(i));
+		}
+		max = Math.max(max, curcontained.size());
+		return max;	
+    }
+}
